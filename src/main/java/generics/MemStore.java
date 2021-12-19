7b3b94cb4ef73a7272c6d6962f6653a58f7b18d5
Master <klimenko161@gmail.com>
2021-12-19 23:14:08 +0300

    generics update

---
+++ src/main/java/generics/MemStore.java
@@ -0,0 +1,28 @@
+package generics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class MemStore<T extends Base> implements Store<T> {
+    private final Map<String, T> storage = new HashMap<>();
+
+    @Override
+    public void add(T model) {
+        storage.put(model.getId(), model);
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        return storage.containsKey();
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public T findById(String id) {
+        return null;
+    }
+}
