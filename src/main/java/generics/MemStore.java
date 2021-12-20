bd3f629aa67b51079198e67ca1b9f449089035ba
Master <klimenko161@gmail.com>
2021-12-20 23:03:10 +0300

    5.2.2. Реализовать Store<T extends Base> [#157 #242282]

--- src/main/java/generics/MemStore.java
+++ src/main/java/generics/MemStore.java
@@ -15,30 +15,16 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, T model) {
-        boolean rsl = false;
-        if (storage.containsKey(id)) {
-            storage.put(id, model);
-            rsl = true;
-        }
-        return rsl;
+        return storage.replace(id, model) != null;
     }
 
     @Override
     public boolean delete(String id) {
-        boolean rsl = false;
-        if (storage.containsKey(id)) {
-            storage.remove(id);
-            rsl = true;
-        }
-        return rsl;
+        return storage.remove(id, findById(id));
     }
 
     @Override
     public T findById(String id) {
-        T rsl = null;
-        if (storage.containsKey(id)) {
-            rsl = storage.get(id);
-        }
-        return rsl;
+        return storage.get(id);
     }
 }
