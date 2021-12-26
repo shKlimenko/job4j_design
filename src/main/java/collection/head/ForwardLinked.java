a74335ed0217852e3044c4cbbc880ee2d7f6e91f
Master <klimenko161@gmail.com>
2021-12-26 20:15:54 +0300

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474]

--- src/main/java/collection/head/ForwardLinked.java
+++ src/main/java/collection/head/ForwardLinked.java
@@ -30,6 +30,16 @@ public class ForwardLinked<T> implements Iterable<T> {
         return deleted;
     }
 
+    public void addFirst(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        node.next = head;
+        head = node;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
---
+++ src/main/java/collection/head/SimpleStack.java
@@ -0,0 +1,14 @@
+package collection.head;
+
+public class SimpleStack<T> {
+
+    private ForwardLinked<T> linked = new ForwardLinked<T>();
+
+    public T pop() {
+        return linked.deleteFirst();
+    }
+
+    public void push(T value) {
+        linked.addFirst(value);
+    }
+}
