423df9b741422a7ebe38110bc4c4e19ea231f42a
Master <klimenko161@gmail.com>
2021-12-31 01:32:48 +0300

    update

--- src/main/java/collection/head/SimpleStack.java
+++ src/main/java/collection/head/SimpleStack.java
@@ -2,13 +2,20 @@ package collection.head;
 
 public class SimpleStack<T> {
 
-    private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private ForwardLinked<T> linked = new ForwardLinked<>();
+    private int size;
 
     public T pop() {
+        size--;
         return linked.deleteFirst();
     }
 
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
