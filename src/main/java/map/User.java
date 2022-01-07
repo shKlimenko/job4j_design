b926f0ff0dc897e3e9ee6b6476f07352abfb9691
Master <klimenko161@gmail.com>
2022-01-07 21:51:55 +0300

    1. Создать модель User [#999]

---
+++ src/main/java/map/User.java
@@ -0,0 +1,13 @@
+package map;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
