99b2d2d72f27bbe5615e58fceed090c7b9ef90db
Master <klimenko161@gmail.com>
2022-01-14 00:07:25 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/io/ResultFile.java
+++ src/main/java/io/ResultFile.java
@@ -4,9 +4,16 @@ import java.io.FileOutputStream;
 
 public class ResultFile {
     public static void main(String[] args) {
+        int size = 5;
+        int[][] tabl = new int[size][size];
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write("Hello, world!".getBytes());
-            out.write(System.lineSeparator().getBytes());
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    tabl[i][j] = (i + 1) * (j + 1);
+                    out.write((tabl[i][j] + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
