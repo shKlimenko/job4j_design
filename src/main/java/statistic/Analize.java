3d9eca4824a99b29381b484ee7f1fe434fde339d
PK rabota <klimenko161@gmail.com>
2022-01-13 18:06:41 +0300

    2. Добавить метод boolean isBinary() [#1712]

---
+++ src/main/java/statistic/Analize.java
@@ -0,0 +1,13 @@
+package statistic;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+
+    }
+
+}
---
+++ src/main/java/statistic/Info.java
@@ -0,0 +1,19 @@
+package statistic;
+
+import java.util.Objects;
+
+public class Info {
+
+    private int added;
+    private int changed;
+    private int deleted;
+
+    public Info(int added, int changed, int deleted) {
+        this.added = added;
+        this.changed = changed;
+        this.deleted = deleted;
+    }
+
+    /* Геттеры, сеттеры, equals() & hashCode() */
+
+}
