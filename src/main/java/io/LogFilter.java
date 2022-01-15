d1964da6249943ec0282d7a59c58441e19d37109
Master <klimenko161@gmail.com>
2022-01-15 22:21:53 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/io/LogFilter.java
@@ -0,0 +1,33 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader log = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = log.readLine()) != null) {
+                String[] splittedLine = line.split(" ");
+                if (Objects.equals(Integer.parseInt(splittedLine[splittedLine.length - 2]), 404)) {
+                    list.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
