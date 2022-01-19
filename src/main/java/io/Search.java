7a4203dd970e5dfbfcb1070e5ad7725d7d5fccb8
PK rabota <klimenko161@gmail.com>
2022-01-19 12:26:11 +0300

    update

--- src/main/java/io/Search.java
+++ src/main/java/io/Search.java
@@ -8,17 +8,6 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
-//    public static void main(String[] args) throws IOException {
-//        Path start = Paths.get(".");
-//        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
-//    }
-//
-//    public static List<Path> search(Path root, Predicate<Path> condition) {
-//        SearchFiles searcher = new SearchFiles(condition);
-//        Files.walkFileTree(root, searcher);
-//        return searcher.getPaths();
-//    }
-
     public static void main(String[] args) throws IOException {
         Path start = Paths.get("c:\\projects\\pogoda");
         Files.walkFileTree(start, new PrintFiles());
