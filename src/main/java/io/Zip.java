672b9078824d4cb912c98d0b56e68207d1f00f50
Master <klimenko161@gmail.com>
2022-01-20 22:24:56 +0300

    update

---
+++ src/main/java/io/Zip.java
@@ -0,0 +1,31 @@
+package io;
+
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public static void packFiles(List<File> sources, File target) {
+
+    }
+
+    public static void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        packSingleFile(
+                new File("./pom.xml"),
+                new File("./pom.zip")
+        );
+    }
+}
