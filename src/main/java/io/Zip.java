c09c2abbb56fe59b8e1293b0c9b18e19b293ad86
Master <klimenko161@gmail.com>
2022-01-22 22:24:42 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/io/Zip.java
+++ src/main/java/io/Zip.java
@@ -1,6 +1,7 @@
 package io;
 
 import java.io.*;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -8,8 +9,19 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public static void packFiles(List<File> sources, File target) {
-
+    public static void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(
+                                        new BufferedOutputStream(
+                                                new FileOutputStream(target)))) {
+            for (Path sPath : sources) {
+                zip.putNextEntry(new ZipEntry(sPath.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(sPath.toFile().getPath()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void packSingleFile(File source, File target) {
@@ -25,16 +37,16 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArgsName argsName = ArgsName.of(args);
         if (args.length != 3 || Paths.get(argsName.get("d")).toFile().isFile()) {
             throw new IllegalArgumentException("Missing arguments. Use "
                     + "java -jar pack.jar ROOT_FOLDER EXCLUDED_FILE OUTPUT_FILE");
         }
 
-        packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
+        List<Path> sources = Search.search(Paths.get(argsName.get("d")),
+                p -> !p.toFile().getName().endsWith(argsName.get("e")));
+
+        packFiles(sources, Paths.get(argsName.get("o")).toFile());
     }
 }
