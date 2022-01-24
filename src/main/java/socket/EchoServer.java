1b29ed08085ccfb488a2e8b1003de8a2a112f885
Master <klimenko161@gmail.com>
2022-01-24 23:09:10 +0300

    bot

--- src/main/java/socket/EchoServer.java
+++ src/main/java/socket/EchoServer.java
@@ -9,17 +9,23 @@ public class EchoServer {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
+                String answer = new String();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("/?msg=Bye")) {
+                        if (str.contains("/msg=Hello")) {
+                            answer = "Hello";
+                        } else if (str.contains("/msg=Exit")) {
                             System.out.println("Closing server");
                             server.close();
+                        } else {
+                            answer = "What?";
                         }
                     }
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(answer.getBytes());
                     out.flush();
                 }
             }
