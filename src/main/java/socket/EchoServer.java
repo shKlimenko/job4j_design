88da98fb2a8cbca24f4bf004280eb3fadc2cfd60
PK rabota <klimenko161@gmail.com>
2022-01-25 12:20:29 +0300

    3. Бот [#7921]

--- src/main/java/socket/EchoServer.java
+++ src/main/java/socket/EchoServer.java
@@ -9,21 +9,20 @@ public class EchoServer {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
-                String answer = new String();
+                StringBuilder msg = new StringBuilder();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("/msg=Hello")) {
-                            answer = "Hello";
-                        } else if (str.contains("/msg=Exit")) {
-                            System.out.println("Closing server");
-                            server.close();
-                        } else {
-                            answer = "What?";
-                        }
+                        msg.append(str);
                     }
+                    if (msg.toString().contains("msg=Exit")) {
+                        System.out.println("Closing server");
+                        server.close();
+                    }
+                    String answer = msg.toString().contains("msg=Hello") ? "Hello" : "What";
+
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     out.write(answer.getBytes());
                     out.flush();
