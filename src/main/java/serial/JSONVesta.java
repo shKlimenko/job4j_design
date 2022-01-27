66594ce6f60817454a9a5d84a05594c67441c813
Master <klimenko161@gmail.com>
2022-01-27 23:13:53 +0300

    upd

--- src/main/java/serial/JSONVesta.java
+++ src/main/java/serial/JSONVesta.java
@@ -8,16 +8,16 @@ import java.util.List;
 
 public class JSONVesta {
     public static void main(String[] args) {
-        JSONObject jsonDashboard = new JSONObject("{\"buttons\":\"14\", \"analogDisplay\":\"true\"}");
+        JSONObject jsonDashboard = new JSONObject("{\"buttons\":\"14\", \"analogDisplay\":true}");
 
         List<String> list = new ArrayList<>();
-        list.add("Student");
-        list.add("Free");
+        list.add("white");
+        list.add("blue");
         JSONArray jsonColors = new JSONArray(list);
 
         final CarModel car = new CarModel(true, 4, "Lada", new Dashboard(17, false), new String[] {"white", "blue"});
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("isLeftSteeringWheel", car.isLeftSteeringWheel());
+        jsonObject.put("LeftSteeringWheel", car.isLeftSteeringWheel());
         jsonObject.put("wheels", car.getWheels());
         jsonObject.put("name", car.getName());
         jsonObject.put("dashboard", jsonDashboard);
