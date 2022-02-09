2c77d04a8ca67cf7aaee50f9b505f2b1ed46b1db
Master <klimenko161@gmail.com>
2022-02-09 23:53:51 +0300

    upd

--- src/main/java/sql/ConnectionDemo.java
+++ src/main/java/sql/ConnectionDemo.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        //Class.forName("org.postgresql.Driver");
+        Class.forName("org.postgresql.Driver");
         String url = "jdbc:postgresql://localhost:5432/idea_db";
         String login = "postgres";
         String password = "password";
