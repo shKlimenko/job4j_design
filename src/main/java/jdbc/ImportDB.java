016c88885833179345a6bc8572370dc3bb76f644
Master <klimenko161@gmail.com>
2022-02-13 16:41:17 +0300

    0.2. PrepareStatement [#379307]

--- src/main/java/jdbc/ImportDB.java
+++ src/main/java/jdbc/ImportDB.java
@@ -22,7 +22,11 @@ public class ImportDB {
     public List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
-            /* rd.lines().forEach(...); */
+            rd.lines().forEach(line -> {
+                String[] splittedRdLine = line.split(";");
+                User user = new User(splittedRdLine[0], splittedRdLine[1]);
+                users.add(user);
+            });
         }
         return users;
     }
@@ -35,7 +39,7 @@ public class ImportDB {
                 cfg.getProperty("jdbc.password")
         )) {
             for (User user : users) {
-                try (PreparedStatement ps = cnt.prepareStatement("insert into users ...")) {
+                try (PreparedStatement ps = cnt.prepareStatement("insert into users (name, email) values (?, ?)")) {
                     ps.setString(1, user.name);
                     ps.setString(2, user.email);
                     ps.execute();
@@ -57,7 +61,7 @@ public class ImportDB {
 
     public static void main(String[] args) throws Exception {
         Properties cfg = new Properties();
-        try (FileInputStream in = new FileInputStream("./app.properties")) {
+        try (FileInputStream in = new FileInputStream("./app2.properties")) {
             cfg.load(in);
         }
         ImportDB db = new ImportDB(cfg, "./dump.txt");
